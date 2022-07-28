cb0531b2f83025844a73f5e93f863ef4d0ed7f23
thanhcongphan2001 <85345954+thanhcongphan2001@users.noreply.github.com>
2022-07-28 18:24:30 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const path = require("path");
 const { engine } = require("express-handlebars");
 //
 const app = express();
-const port =process.env.PORT || 3000;
+// const port =process.env.PORT || 3000;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3002;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 const route = require("./routes");
 //   "start": "nodemon --inspect src/index.js",
@@ -36,6 +38,10 @@ app.set("views", path.join(__dirname, 'resources' , 'views'));
 
 route(app);
 
-app.listen(port, () => {
-  console.log(`Example app listening ${port}`);
+// app.listen(port, () => {
+//   console.log(`Example app listening ${port}`);
+// });
+
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
