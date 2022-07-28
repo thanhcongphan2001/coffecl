51fb9fd557e4c50f104b4500a4989e6dfb4a2205
Your Name <you@example.com>
2022-07-28 16:42:36 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -38,5 +38,5 @@ app.set("views", path.join(__dirname, 'resources' , 'views'));
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening ${port}`);
 });
